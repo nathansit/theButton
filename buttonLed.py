dc935a1aa31af03628a71898820cd08ce127b038
Nathan Sit <nsit.earth@gmail.com>
2023-09-15 00:03:36 -0700

    not working but almost there make a stack

--- buttonLed.py
+++ buttonLed.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
 import logging
+import datetime
 logger = logging.getLogger()
 logger.setLevel("DEBUG")
 
@@ -14,7 +15,7 @@ buttonPin = 12
 broker="homeassistant.local"
 
 def on_publish(client, userdata, result):
-	print("data publish \n")
+    print("data publish \n")
 
 client = mqtt.Client("python1")
 client.on_publish = on_publish
@@ -24,20 +25,32 @@ client.username_pw_set("localmqtt", "Ezekiel 10:19")
 
 #GPIO.setup(ledPin, GPIO.OUT)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-while True:
-	buttonState = GPIO.input(buttonPin)
-#	print(buttonState)
-	if buttonState == False:
-#		GPIO.output(ledPin, GPIO.HIGH)
-		# send a message
-		client.connect(broker)
-		client.publish("pi/nathan/button/1", "single_click")
-		time.sleep(0.2)
-		client.disconnect()
-		pass
-	else:
-		# client.publish("pi/nathan/button/1", "single_click")
-		time.sleep(0.05)
 
-		pass
+# def countClicks() # count the number of clicks and then use that to determine the funciton
+
+# time between last press
+DOWN = False
+UP = True
+
+prevState = UP
+currState = UP
+
+# modified to detect a change in the state of button
+while True:
+    time.sleep(0.1)
+    currState =  GPIO.input(buttonPin)
+#	print(buttonState)
+    # if button state is switched
+    if currState != prevState:
+
+        # reset the prevState
+        prevState = currState
+        print("button changed!")
+        # send a message
+#		client.connect(broker)
+#		client.publish("pi/nathan/button/1", "single_click")
+#		client.disconnect()
+    else:
+        # client.publish("pi/nathan/button/1", "single_click")
+        pass
 
